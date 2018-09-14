457c60d348afedbfabcc1a18f7cedc1d6070b9f9
xiaohuoni <448627663@qq.com>
2018-09-14 10:57:17 +0800

    提交ModalTarget，简单的说就是全局modal的控制器

---
+++ rn/components/ModalTarget/usage/index.js
@@ -0,0 +1,10 @@
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import { ModalTarget } from "whale-rn";
+
+AppRegistry.registerComponent(appName, () => (
+  <ModalTarget>
+    <App />
+  </ModalTarget>
+));
