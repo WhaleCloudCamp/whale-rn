910dba8d9a20f568bd6762141a5caedf6e7790a8
xiaohuoni <448627663@qq.com>
2018-09-14 18:04:38 +0800

    完成ModalView

---
+++ rn/components/ModalView/index.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ReactNative, {
+  StyleSheet,
+  View,
+  PanResponder,
+  Platform,
+  ViewPropTypes
+} from "react-native";
+
+import Theme from "../../themes";
+
+export default class ModalView extends Component {
+  static propTypes = {
+    style: ViewPropTypes.style,
+    modal: PropTypes.bool,
+    modalOpacity: PropTypes.number,
+    onAppearCompleted: PropTypes.func,
+    onDisappearCompleted: PropTypes.func,
+    onCloseRequest: PropTypes.func
+  };
+
+  static defaultProps = {
+    modal: false
+  };
+
+  constructor(props) {
+    super(props);
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (e, gestureState) => true,
+      onPanResponderGrant: (e, gestureState) =>
+        (this.touchStateID = gestureState.stateID),
+      onPanResponderRelease: (e, gestureState) =>
+        this.touchStateID == gestureState.stateID ? this.closeRequest() : null
+    });
+  }
+
+  componentWillMount() {
+    if (Platform.OS === "android") {
+      let BackHandler = ReactNative.BackHandler
+        ? ReactNative.BackHandler
+        : ReactNative.BackAndroid;
+      this.backListener = BackHandler.addEventListener(
+        "hardwareBackPress",
+        () => {
+          this.closeRequest();
+          return true;
+        }
+      );
+    }
+  }
+
+  componentDidMount() {
+    let { onAppearCompleted } = this.props;
+    onAppearCompleted && onAppearCompleted();
+  }
+
+  componentWillUnmount() {
+    this.removeBackListener();
+  }
+
+  removeBackListener() {
+    if (this.backListener) {
+      this.backListener.remove();
+      this.backListener = null;
+    }
+  }
+
+  close() {
+    if (this.closed) return true;
+    this.closed = true;
+    this.removeBackListener();
+    let { onDisappearCompleted } = this.props;
+    onDisappearCompleted && onDisappearCompleted();
+    return true;
+  }
+
+  closeRequest() {
+    let { modal, onCloseRequest } = this.props;
+    if (onCloseRequest) onCloseRequest(this);
+    else if (!modal) this.close();
+  }
+
+  render() {
+    let { style, modalOpacity } = this.props;
+    style = [{ backgroundColor: "rgba(0, 0, 0, 0)", flex: 1 }].concat(style);
+    return (
+      <View style={styles.globalmodal}>
+        <View
+          style={[
+            styles.globalmodal,
+            {
+              backgroundColor: "#000",
+              opacity: modalOpacity || Theme.opacity_disabled
+            }
+          ]}
+          {...this.panResponder.panHandlers}
+        />
+
+        <View style={style} pointerEvents="box-none">
+          {this.props.children}
+        </View>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  globalmodal: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+});
---
+++ rn/components/ModalView/usage/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Button, ModalBasics, ModalView } from "../../index";
+
+export default class ModalViewExample extends Component {
+  showModal(modal, text, modalOpacity = 0.7) {
+    let modalView = (
+      <ModalView
+        style={{ alignItems: "center", justifyContent: "center" }}
+        modal={modal}
+        modalOpacity={modalOpacity}
+        ref={v => (this.modalView = v)}
+      >
+        <View
+          style={{
+            backgroundColor: "#00FF00",
+            padding: 40,
+            borderRadius: 15,
+            alignItems: "center"
+          }}
+        >
+          {modal ? (
+            <TouchableOpacity
+              onPress={() => this.modalView && this.modalView.close()}
+            >
+              <Text>点我关闭</Text>
+            </TouchableOpacity>
+          ) : (
+            <Text>任意点击关闭</Text>
+          )}
+        </View>
+      </ModalView>
+    );
+    ModalBasics.show(modalView);
+  }
+
+  render() {
+    return (
+      <View>
+        <Button onPress={() => this.showModal(true, "isModal", 0.5)}>
+          显示自定义弹窗，点击按钮关闭
+        </Button>
+
+        <Button onPress={() => this.showModal(false, "isNoModal", 0.3)}>
+          显示模态窗，点击背景关闭
+        </Button>
+      </View>
+    );
+  }
+}
