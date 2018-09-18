e16fd0f75c77263011e96d0e2c6c5e515825bc0a
ashoka <savebtc11@gmail.com>
2018-09-18 18:15:26 +0800

    添加 pagination 组件

---
+++ rn/components/Pagination/usage/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native';
+import Pagination from '..';
+
+export default class PaginationDemo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            current: 0,
+            total: 5
+        }
+    }
+
+    onPreviousPress = () => {
+        this.setState({
+            current: Math.max(this.state.current - 1, 0)
+        })
+    }
+
+    onNextPress = () => {
+        this.setState({
+            current: Math.min(this.state.current + 1, this.state.total - 1)
+        })
+    }
+
+    render() {
+        const { current, total } = this.state;
+        return (
+            <View style={{ flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }}>
+                <Pagination
+                    current={current}
+                    total={total}
+                />
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <TouchableOpacity style={styles.button} onPress={this.onPreviousPress}>
+                        <Text>上一页</Text>
+                    </TouchableOpacity>
+                    <View style={{ paddingHorizontal: 10, backgroundColor: '#0084FF' }}>
+                        <Pagination
+                            current={current}
+                            total={total}
+                            inActiveColor='rgba(255,255,255,0.5)'
+                            activeColor='#fff'
+                        />
+                    </View>
+                    <TouchableOpacity style={styles.button} onPress={this.onNextPress}>
+                        <Text>下一页</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        marginHorizontal: 20,
+        borderWidth: 1,
+        borderColor: 'black'
+    }
+})
