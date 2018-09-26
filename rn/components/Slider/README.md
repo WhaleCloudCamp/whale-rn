12f792579fdde70be15543d723dba41798c31daf
MissDelia <503590360@qq.com>
2018-09-26 15:14:41 +0800

    add Slider Component

---
+++ rn/components/Slider/README.md
@@ -0,0 +1,26 @@
+# 使用方法
+
+## 引用
+import Slider from './Slider';
+
+## 使用
+```jsx
+    <View>
+        <Text>Default settings</Text>
+        <Slider />
+    </View>
+```
+
+## 参数含义
+Params | Type | Default | Note
+----|-----|------|------
+| min    |  Number     | 0     | 最小值 |
+| max    |  Number     | 100    | 最大值 |
+| step    |  Number     | 1    | 步长，取值必须大于 0，并且可被 (max - min) 整除。 |
+| value    |  Number  |     | 设置当前取值。 |
+| defaultValue    |  Number   | 0     | 设置初始取值。|
+| disabled    |  Boolean     | false    | 值为 `true` 时，滑块为禁用状态 |
+| onChange    |  Function     | Noop    | 当 Slider 的值发生改变时，会触发 onChange 事件，并把改变后的值作为参数传入 |
+| maximumTrackTintColor  |  String     | `#108ee9`（RN)    | 底部背景色 |
+| minimumTrackTintColor  |  String     | `#ddd` (RN)   | 当前选中部分的颜色 |
+| onAfterChange    |  Function     | Noop    | 与 `ontouchend` 触发时机一致，把当前值作为参数传入 |
---
+++ rn/components/Slider/usage/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Slider from '../index';
+
+export default class SliderDemo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      changingValue: 0.25,
+      changedValue: 0.15,
+      minMaxValue: 0,
+    };
+  }
+
+  handleChange = value => {
+    this.setState({
+      changingValue: value,
+    });
+  };
+
+  onAfterChange = value => {
+    this.setState({
+      changedValue: value,
+    });
+  };
+
+  minMaxChange = value => {
+    this.setState({
+      minMaxValue: value,
+    });
+  };
+
+  render() {
+    return (
+      <View>
+        <View>
+          <Text>Default settings</Text>
+          <Slider />
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <Text>Default value settings</Text>
+          <Slider showValue max={100} step={1} />
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <Text>Initial value: 0.5</Text>
+          <Slider defaultValue={0.5} />
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <Text>min: 0, max: 1, current Value: {this.state.minMaxValue}</Text>
+          <Slider
+            min={0}
+            max={1}
+            onAfterChange={value => this.minMaxChange(value)}
+          />
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <Text>step: 0.25</Text>
+          <Slider step={0.25} value={0.25} />
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <Text>disabled</Text>
+          <Slider disabled defaultValue={0.25} />
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <Text>onChange value: {this.state.changingValue}</Text>
+          <Slider
+            defaultValue={0.25}
+            onChange={value => this.handleChange(value)}
+          />
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <Text>onAfterChange value: {this.state.changedValue}</Text>
+          <Slider
+            defaultValue={0.15}
+            onAfterChange={value => this.onAfterChange(value)}
+          />
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <Text>custom color: </Text>
+          <Slider
+            defaultValue={0.15}
+            minimumTrackTintColor="red"
+            maximumTrackTintColor="blue"
+          />
+        </View>
+      </View>
+    );
+  }
+}
