0ed8ccc2c3e9ad43d4340947c3709b35a07edb12
lwq321 <825780118@qq.com>
2018-09-28 14:07:43 +0800

    用例文档补全

--- rn/components/CheckBox/usage/index.js
+++ rn/components/CheckBox/usage/index.js
@@ -5,10 +5,31 @@ import CheckBox from '../index';
 export default class CheckBoxDemo extends React.Component {
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, alignItems: 'center' }}>
         <CheckBox
+          stytles={{ margin: 10 }}
+          disable={false}
           normalImage={require('../img/checkbox_selected_normal.png')}
           checkedImage={require('../img/checkbox_unselected_normal.png')}
+          onChange={ischeck => {
+            console.warn(ischeck);
+          }}
+        />
+
+        <CheckBox
+          stytles={{ margin: 10 }}
+          disable
+          isChecked={false}
+          normalImage={require('../img/checkbox_selected_normal.png')}
+          checkedImage={require('../img/checkbox_unselected_normal.png')}
+          onChange={ischeck => {
+            console.warn(ischeck);
+          }}
+        />
+        <CheckBox
+          stytles={{ margin: 10 }}
+          disable
+          isChecked
           onChange={ischeck => {
             console.warn(ischeck);
           }}
