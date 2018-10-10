527265e443cb7e190cd104183eb58a0adafb8a60
Jeno <huanghuihong@dingtalk.com>
2018-10-10 16:44:13 +0800

    组件用例完善

--- rn/components/Tabs/index.js
+++ rn/components/Tabs/index.js
@@ -6,6 +6,7 @@ import Themes from '../../themes';
 
 export default class TabBar extends React.Component {
   static propTypes = {
+    tabsArray: PropTypes.array.isRequired,
     barTintColor: PropTypes.string,
     tintColor: PropTypes.string,
     unselectedTintColor: PropTypes.string,
@@ -15,7 +16,7 @@ export default class TabBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTab: 1,
+      selectedTab: 0,
     };
   }
 
@@ -28,6 +29,7 @@ export default class TabBar extends React.Component {
 
   render() {
     const {
+      tabsArray,
       barTintColor = Themes.fill_base,
       tintColor = '#108ee9',
       unselectedTintColor = '#888888',
@@ -35,29 +37,29 @@ export default class TabBar extends React.Component {
 
     const style = { backgroundColor: barTintColor };
 
+    const renderTabItems = () =>
+      tabsArray.map((item, index) => {
+        const { title } = item;
+        return (
+          <TabItem
+            key={`r-${index}`}
+            title={title}
+            selected={this.state.selectedTab === index}
+            onPress={() => this.onChangeTab(index)}
+            tintColor={tintColor}
+            unselectedTintColor={unselectedTintColor}
+          />
+        );
+      });
+
     return (
-      <View style={[styles.tabs, style]}>
-        <TabItem
-          title="选项"
-          selected={this.state.selectedTab === 1}
-          onPress={() => this.onChangeTab(1)}
-          tintColor={tintColor}
-          unselectedTintColor={unselectedTintColor}
-        />
-        <TabItem
-          title="选项"
-          selected={this.state.selectedTab === 2}
-          onPress={() => this.onChangeTab(2)}
-          tintColor={tintColor}
-          unselectedTintColor={unselectedTintColor}
-        />
-        <TabItem
-          title="选项"
-          selected={this.state.selectedTab === 3}
-          onPress={() => this.onChangeTab(3)}
-          tintColor={tintColor}
-          unselectedTintColor={unselectedTintColor}
-        />
+      <View
+        style={[
+          styles.tabs,
+          style,
+        ]}
+      >
+        {renderTabItems()}
       </View>
     );
   }
