5752d76892e4af9bf1d4cc8291c27a78e099b597
lwq321 <825780118@qq.com>
2018-10-19 17:39:22 +0800

    noticebar走马灯效果的开关，用例和文档

--- rn/components/NoticeBar/index.js
+++ rn/components/NoticeBar/index.js
@@ -4,6 +4,8 @@ import {
   View,
   ViewPropTypes,
   Text,
+  Animated,
+  Easing,
   TouchableWithoutFeedback,
   StyleSheet,
 } from 'react-native';
@@ -18,13 +20,45 @@ export default class NoticeBar extends React.Component {
     onClick: PropTypes.func,
     style: ViewPropTypes.style,
     textStyle: Text.propTypes.style,
+    disScrolled: PropTypes.bool,
+    time: PropTypes.number,
   };
 
   constructor(props) {
     super(props);
     this.state = {
       close: false,
+      translateX: new Animated.Value(0),
     };
+  }
+
+  static defaultProps = {
+    disScrolled: true,
+    time: 5000,
+  };
+
+  layout = e => {
+    if (this.props.disScrolled) {
+      this.showHeadBar(0, 1, e.layout.width);
+    }
+  };
+
+  showHeadBar(index, count, textWidth) {
+    index++;
+
+    Animated.timing(this.state.translateX, {
+      toValue: -textWidth,
+      duration: this.props.time, //动画时间
+      Easing: Easing.linear,
+      delay: 1500, //文字停留时间
+    }).start(() => {
+      //每一个动画结束后的回调
+      if (index >= count) {
+        index = 0;
+        this.state.translateX.setValue(0);
+      }
+      this.showHeadBar(index, count, textWidth); //循环动画
+    });
   }
 
   closeClick = () => {
@@ -38,31 +72,54 @@ export default class NoticeBar extends React.Component {
 
     const {
       icon = require('../../icons/NoticeBar_Horn.png'),
+      closePic = require('../../icons/NoticeBar_Dark_close.png'),
       title,
       showClose = false,
       onClick,
       style,
+      styleBar,
       textStyle,
+      styleLeftImgView,
+      disScrolled,
     } = this.props;
 
     const main = () => (
       <View style={[styles.noticeBar, style]}>
-        <Image style={styles.icon} source={icon} />
-        <Text
-          style={[styles.title, textStyle]}
-          numberOfLines={1}
-          onPress={onClick}
+        <Animated.View
+          style={[
+            styles.wrapper,
+            {
+              transform: [
+                {
+                  translateX: this.state.translateX,
+                },
+              ],
+            },
+          ]}
         >
-          {title}
-        </Text>
+          <View style={[styles.bar, styleBar]}>
+            <Text
+              style={[styles.title, textStyle]}
+              onPress={onClick}
+              numberOfLines={1}
+              onLayout={({ nativeEvent: e }) => {
+                this.layout(e);
+              }}
+            >
+              {title}
+            </Text>
+          </View>
+        </Animated.View>
+
         {showClose && (
           <TouchableWithoutFeedback onPress={this.closeClick}>
-            <Image
-              style={styles.close}
-              source={require('../../icons/NoticeBar_Dark_close.png')}
-            />
+            <Image style={styles.close} source={closePic} />
           </TouchableWithoutFeedback>
         )}
+
+        <View style={[styles.leftImgView, styleLeftImgView]}>
+          <Image style={styles.icon} source={icon} />
+        </View>
       </View>
     );
 
@@ -77,17 +134,39 @@ const styles = StyleSheet.create({
     backgroundColor: Themes.notice_bar_fill,
     alignItems: 'center',
   },
+  bar: {
+    height: Themes.notice_bar_height,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: Themes.notice_bar_fill,
+  },
+  wrapper: {
+    flex: 1,
+    marginLeft: 40,
+  },
+  leftImgView: {
+    position: 'absolute',
+    left: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: Themes.notice_bar_fill,
+    height: '100%',
+    width: 40,
+  },
   icon: {
     width: 16,
     height: 16,
     marginLeft: 12,
   },
   title: {
-    marginLeft: 12,
+    position: 'absolute',
+    left: 0,
+
     marginRight: 5,
     fontSize: 14,
     color: '#F86E21',
-    flex: 1,
+    flexWrap: 'nowrap',
   },
   close: {
     tintColor: '#F86E21',
--- rn/components/NoticeBar/usage/index.js
+++ rn/components/NoticeBar/usage/index.js
@@ -10,11 +10,27 @@ export default class NoticeBarDemo extends React.Component {
         <NoticeBar
           title="这条通告栏不带关闭按钮这条通告栏不带关闭按钮这条通告栏不带关闭按钮"
           showClose={false}
+          onClick={() => {
+            console.warn('点击');
+          }}
         />
         <NoticeBar
           style={{ marginTop: 10 }}
-          title="这条通告栏带关闭按钮这条通告栏带关闭按钮这条通告栏带关闭按钮"
+          title="通知"
           showClose
+          time={1000}
+          onClick={() => {
+            console.warn('点击');
+          }}
+        />
+        <NoticeBar
+          style={{ marginTop: 10 }}
+          title="这条通告栏带关闭按钮这条通告栏"
+          showClose
+          disScrolled={false}
+          onClick={() => {
+            console.warn('点击');
+          }}
         />
       </Page>
     );
