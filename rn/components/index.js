ab73247a1569bb2cc947fe7dc1a7095741236d99
xiaohuoni <448627663@qq.com>
2018-10-19 14:53:46 +0800

    发布测试版7

--- rn/components/index.js
+++ rn/components/index.js
@@ -1,29 +1,31 @@
-import ActionSheet from './ActionSheet';
-import ActivityIndication from './ActivityIndication';
-import AreaSelect from './AreaSelect';
-import Badge from './Badge';
-import Button from './Button';
-import CheckBox from './CheckBox';
-import Input from './Input';
-import InputItem from './InputItem';
-import List from './List';
-import Label from './Label';
-import MenuPicker from './MenuPicker';
-import Modal from './Modal';
-import NavBar from './NavBar';
-import NoticeBar from './NoticeBar';
-import Pagination from './Pagination';
-import Progress from './Progress';
-import Radio from './Radio';
-import SearchBar from './SearchBar';
-import Slider from './Slider';
-import Stepper from './Stepper';
-import Switch from './Switch';
-import TabBar from './TabBar';
-import Tabs from './Tabs';
-import Tag from './Tag';
-import TextareaItem from './TextareaItem';
-import Toast from './Toast';
+import ActionSheet from "./ActionSheet";
+import ActivityIndication from "./ActivityIndication";
+import AreaSelect from "./AreaSelect";
+import Badge from "./Badge";
+import Button from "./Button";
+import CheckBox from "./CheckBox";
+import Input from "./Input";
+import InputItem from "./InputItem";
+import List from "./List";
+import Label from "./Label";
+import MenuPicker from "./MenuPicker";
+import Modal from "./Modal";
+import NavBar from "./NavBar";
+import NoticeBar from "./NoticeBar";
+import Pagination from "./Pagination";
+import Progress from "./Progress";
+import Radio from "./Radio";
+import SearchBar from "./SearchBar";
+import Slider from "./Slider";
+import Stepper from "./Stepper";
+import Switch from "./Switch";
+import TabBar from "./TabBar";
+import Tabs from "./Tabs";
+import Tag from "./Tag";
+import TextareaItem from "./TextareaItem";
+import Toast from "./Toast";
+import Flex from "./Flex";
+import ItemGrid from "./ItemGrid";
 
 const WhaleRN = {
   ActionSheet,
@@ -52,6 +54,8 @@ const WhaleRN = {
   Tag,
   TextareaItem,
   Toast,
+  Flex,
+  ItemGrid
 };
 
 module.exports = WhaleRN;
